e073e2bfcef8b3472e2869fd2baf2d372f078b22
d-brand <kawasaki@d-brand.jp>
2020-07-20 18:43:35 +0900

    ファビコン追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :schedules
   root 'schedules#show'
   # get "/" => "schedules#index"
   get "schedules/kiyaku" => "schedules#kiyaku"
+  resources :answers
+  resources :schedules
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
